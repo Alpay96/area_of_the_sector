c5fec0cf4c6ef6f30c25733f291f14856b3d76d9
alpay <alpay@LAPTOP-U4PI6GNT>
2022-11-06 22:20:20 +0300

    https://app.patika.dev/majin

---
+++ area_of_the_sector/src/area_of_the_sector/Main.java
@@ -0,0 +1,20 @@
+package area_of_the_sector;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		Scanner scan = new Scanner(System.in);
+		System.out.print("Enter the radius of the circle: ");
+		int r = scan.nextInt();
+		
+		System.out.print("Enter the center angle: ");
+		int a = scan.nextInt();
+
+		double pi = 3.14;
+		double area = pi * r * r * (a / 360d);
+		System.out.println("Area: " + area);
+	}
+}
---
+++ area_of_the_sector/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author alpay
+ *
+ */
+module area_of_the_sector {
+}
